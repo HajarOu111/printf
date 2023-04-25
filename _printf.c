6dc240c92f02764bb7bdbdf46af4b8c43fd576dc
HAJAR OUAKRIM <assma941@gmail.com>
2023-04-25 08:11:57 -0700

    Printf function

--- _printf.c
+++ _printf.c
@@ -3,65 +3,45 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function
+ * _printf - Printf function.
  * @format: format.
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
-
-	if (format == NULL)
-		return (-1);
-
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
-	}
-
-	print_buffer(buffer, &buff_ind);
-
-	va_end(list);
-
-	return (printed_chars);
+va_list list;
+va_start(list, format);
+int printed_chars = vsnprintf(NULL, 0, format, list);
+va_end(list);
+if (printed_chars < 0)
+{
+return (-1);
+}
+char *buffer = malloc(printed_chars + 1);
+if (buffer == NULL)
+{
+return (-1);
+}
+va_start(list, format);
+printed_chars = vsnprintf(buffer, printed_chars + 1, format, list);
+va_end(list);
+write(1, buffer, printed_chars);
+free(buffer);
+return (printed_chars);
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
+ * print_buffer - Prints the contents of the buffer if it exists.
+ * @buffer: Array of chars.
  * @buff_ind: Index at which to add next char, represents the length.
  */
+
 void print_buffer(char buffer[], int *buff_ind)
 {
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
+for (int i = 0; i < *buff_ind; i++)
+{
+putchar(buffer[i]);
+}
+*buff_ind = 0;
 }
 
